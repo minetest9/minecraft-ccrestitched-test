d1c48578d7b839a6d2d293a3243a9145d3b830d1
minetest9 <136476484+minetest9@users.noreply.github.com>
2023-06-13 17:47:30 +0200

    Update diamond mining.lua

--- diamond mining.lua
+++ diamond mining.lua
@@ -1,21 +1,13 @@
-local fruits = { "minecraf:diamond_ore", "minecraf:lapis_ore", "minecraf:coal_ore", "minecraf:emerald_ore", "minecraf:redstone_ore", "minecraf:_ore" }
-for _, fruit in pairs(fruits) do
-   if fruit == "pear" then
-      do print("We Found it!")
-      break
-   end
-   else print("Oh no, keep traversing!")
-   end
-end
-
 
 fuel = turtle.getFuelLevel()
 beginfuel = turtle.getFuelLevel()
+fuelused = 0
 while(fuel > 1)
 do
   fuel = turtle.getFuelLevel()
   if fuel <= 30
     turtle.refuel([1]) 
+    fuelused = fuelused+1
   else
       if turtle.detect() then
          turtle.dig()
@@ -24,7 +16,6 @@ do
          turtle.digUp()
           end
       if turtle.detectDown() then
-        
          turtle.digDown()
           end
     end
